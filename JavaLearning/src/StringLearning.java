fb2eb7fffc450b7128d2e9c6882ec989b383e548
robinv312 <robinv312@gmail.com>
2026-03-20 14:44:42 +0530

    String chnages

---
+++ JavaLearning/src/StringLearning.java
@@ -0,0 +1,11 @@
+public class StringLearning {
+
+    String onm= "Hello";
+
+
+    public static void main(String [] args){
+        StringLearning obj = new StringLearning();
+        System.out.println(obj.onm+=" world");
+
+    }
+}
