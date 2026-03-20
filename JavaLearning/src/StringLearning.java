05a88d874c82ffa8613599db9de861315ca7c5d0
robinv312 <robinv312@gmail.com>
2026-03-20 14:56:52 +0530

    String chnages

--- JavaLearning/src/StringLearning.java
+++ JavaLearning/src/StringLearning.java
@@ -7,5 +7,13 @@ public class StringLearning {
         StringLearning obj = new StringLearning();
         System.out.println(obj.onm+=" world");
 
+        StringBuffer sb = new StringBuffer("My name is");
+        sb.append("Serah");
+        System.out.println(sb);
+
+        StringBuilder sBuilder = new StringBuilder("My name is");
+        sb.append("Mereena");
+        System.out.println(sBuilder);
+
     }
 }
